74288c4c067baef3add7fe7725a92d677f8df064
Suzanna Whiteside <SuzannaMW@gmail.com>
2013-04-27 13:44:24 -0700

    persistent connection still echoing, but now it can do many in a row.  There is an issue where if the phrase is long it gets all funky.

--- proxy-connection.cc
+++ proxy-connection.cc
@@ -1,4 +1,5 @@
 #include "proxy-connection.h"
+#include "http-response.h"
 
 void* socketConnection( void* parameters){
   fprintf(stderr, "You are a connnection");
@@ -9,17 +10,55 @@ void* socketConnection( void* parameters){
 
   char recvbuf[DEFAULT_BUFLEN];
   int recvbuflen = DEFAULT_BUFLEN;
+  struct timeval tv;
+  fd_set readfds;
+  int n, rv;
 
-  int rv = recv(p->sockfd,recvbuf,recvbuflen, 0);
-  if (rv < 0){
-    fprintf(stderr, "recv failed\n");
-    return NULL;
+  while(true)
+  {
+    //persistent connection:  continue to listen to the socket at all times.
+    // clear the set ahead of time
+    FD_ZERO(&readfds);
+
+    // add our descriptors to the set
+    FD_SET(p->sockfd, &readfds);
+    //for now
+    n = p->sockfd + 1;
+    tv.tv_sec = 10;
+    tv.tv_usec = 500000;
+    rv = select(n, &readfds, NULL, NULL, &tv);
+
+    if (rv == -1) {
+      perror("select"); // error occurred in select()
+    } else if (rv == 0) {
+      fprintf(stderr, "Timeout occurred!  No data after 10.5 seconds.\n");
+    } else {
+      // one or both of the descriptors have data
+      if (FD_ISSET(p->sockfd, &readfds)) {
+	rv = recv(p->sockfd, recvbuf, recvbuflen, 0);
+	if (rv < 0){
+	  fprintf(stderr, "recv failed\n");
+	  return NULL;
+	}
+	// echo response for now                                                                                                                                      
+	string r(recvbuf);
+	string message = "You said: " + r;
+	//char sendbuf[DEFAULT_BUFLEN];
+	//	strcpy (&sendbuf, message.c_str());
+	send(p->sockfd,message.c_str(),strlen(message.c_str())+1,0);
+
+      }
+
+    }
   }
 
-  // echo response for now                                                                                                                                                          
-  string r(recvbuf);
-  string message = "You said: " + r;
-  send(p->sockfd,message.c_str(),(int)strlen( message.c_str()),0);
+  //  int rv = recv(p->sockfd,recvbuf,recvbuflen, 0);
+  //if (rv < 0){
+  //fprintf(stderr, "recv failed\n");
+  //return NULL;
+  //}
+
+
 
   return NULL;
 }
